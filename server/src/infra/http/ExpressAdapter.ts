2b47ef8e450d95fba872332b3d7b1a45ed6558e8
Júlio Freitas <jcsr.frts@gmail.com>
2023-07-05 12:10:43 -0300

    update cors access

--- server/src/infra/http/ExpressAdapter.ts
+++ server/src/infra/http/ExpressAdapter.ts
@@ -8,7 +8,7 @@ export class ExpressAdapter implements Http {
         this.app = express();
         this.app.use(express.json());
         this.app.use(function (req: any, res: any, next: any) {
-            res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+            res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
             res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
             next();
