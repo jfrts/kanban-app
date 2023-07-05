793d7201a6d12370af049e4fc3bff073db3eb36e
Júlio Freitas <jcsr.frts@gmail.com>
2023-07-05 10:02:55 -0300

    feat: config to deploy

--- server/src/infra/database/PgPromiseConnection.ts
+++ server/src/infra/database/PgPromiseConnection.ts
@@ -5,12 +5,14 @@ export class PgPromiseConnection implements Connection {
     connection: any;
 
     constructor() {
-        this.connection = pgp()("postgres://postgres:postgres123@localhost:5432/branas");
+        this.connection = pgp()("postgres://app_branas_course_user:AmIITgb19mDPwRhyuVzlmw0YQT9Km3l5@dpg-ciimefl9aq012et04qa0-a/app_branas_course");
+        // this.connection = pgp()("postgres://postgres:postgres123@localhost:5432/branas");
     };
 
     query(statement: string, params: unknown): Promise<unknown> {
         return this.connection.query(statement, params);
     }
+    
     close(): Promise<void> {
         return this.connection.$pool.end();
     }
